4821530a53cc4d4a5d245a1334278d6d45a6b006
ayush <rathoreayush714@gmail.com>
2024-06-21 19:22:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template,redirect
+import joblib
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+import warnings
+warnings.filterwarnings('ignore')
+
+app = Flask(__name__)
+knn_loaded = joblib.load('./static/KNN.joblib')
+dtr_loaded = joblib.load('./static/Decision_Tree.joblib')
+gbr_loaded = joblib.load('./static/Gradient_Boosting.joblib')
+mlp_loaded = joblib.load('./static/MLP.joblib')
+rf_loaded = joblib.load('./static/Random_Forest.joblib')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = []
+    for x in request.form.values():
+        try:
+            int_features.append(int(float(x)))
+        except ValueError:
+            print(f"Skipping invalid value: {x}")
+
+    print(int_features)
+    mo = int_features[0]
+    prediction = []
+    output = 0
+    if mo == 1:
+        int_features = int_features[1:]
+        final_features = np.array(int_features)
+        prediction = knn_loaded.predict([final_features])
+    elif mo == 2:
+        int_features = int_features[1:]
+        final_features = [np.array(int_features)]
+        prediction = rf_loaded.predict(final_features)
+    elif mo == 3:
+        int_features = int_features[1:]
+        final_features = [np.array(int_features)]
+        prediction = gbr_loaded.predict(final_features)
+    elif mo == 4:
+        int_features = int_features[1:]
+        final_features = [np.array(int_features)]
+        prediction = dtr_loaded.predict(final_features)
+    elif mo == 5:
+        int_features = int_features[1:]
+        final_features = [np.array(int_features)]
+        prediction = mlp_loaded.predict(final_features)
+        
+    output = round(prediction[0], 2)
+    print(output)
+    
+    return render_template('post.html',prediction_text= "The predicted Compressive Strength of sample is {}kg/m3.".format(output))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
